50556529fda88420f7bb6bddbc0cf10c132262ce
sakshi <goradsakshi956@gmail.com>
2026-01-22 21:37:53 +0530

    day011.c

---
+++ day011.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+int n;
+scanf("%d", &n);
+int *a = (int *)malloc(n * sizeof(int));
+int sum = 0;
+for (int i = 0; i < n; i++)
+{
+scanf("%d", &a[i]);
+sum += a[i];
+}
+printf("%d", sum);
+free(a);
+return 0;
+}
